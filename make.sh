934f79858f64f4b0ee2e1b1ffab7456bc745c11a
hossein <hosteam01@gmail.com>
2023-08-08 14:20:19 +0330

    added `-f` flag (force)
    + `-f` flag: remove and install the script in one go
    + better code style
    + updated link (github username)

--- make.sh
+++ make.sh
@@ -1,49 +1,78 @@
 #!/bin/bash
 set -e
 
-ver_number="0.2"
+ver_number="0.8"
 
+# apply {{{
 function applyit () {
-# check any config file exists
-if [[ -f "$HOME/.zshrc" ]]; then 
-  # check any backup file exists
-  if [[ -f "$HOME/.zshrc.bak" ]]; then
-    echo "remove old backup.."
-    rm -f $HOME/.zshrc.bak
-  fi
-  # backup the current .zshrc file if exists
-  echo "backup ${HOME}/zshrc file.."
-  mv -f $HOME/.zshrc $HOME/.zshrc.bak
-  echo ""
-fi
+	func_name="apply:"
+	# check any config file exists
+	if [[ -f "${HOME}/.zshrc" ]]; then 
+		# check any backup file exists
+		if [[ -f "${HOME}/.zshrc.bak" ]]; then
+			install_rc_file=1
+			echo "${func_name} .zshrc backup exists"
+		else
+			install_rc_file=0
+			echo "backup ${HOME}/zshrc file.."
+			mv -f ${HOME}/.zshrc ${HOME}/.zshrc.bak
+			echo ""
+		fi
+	fi
 
-# check any zsh.d directory exists
-if [[ -d "$HOME/.zsh.d" ]]; then
-  # check any backup zsh.d directory exists
-  if [[ -d "$HOME/.zsh.d.bak" ]]; then
-    echo "remove old backup.."
-    rm -rf $HOME/.zsh.d.bak
-  fi
-  # backup the current .zsh.d directory if exists
-  echo "backup ${HOME}/zsh.d directory.."
-  cp -rf $HOME/.zsh.d $HOME/.zsh.d.bak
-  rm -rf $HOME/.zsh.d
-  echo ""
-fi
+	# check any zsh.d directory exists
+	if [[ -d "${HOME}/.zsh.d" ]]; then
+		# check any backup zsh.d directory exists
+		if [[ -d "${HOME}/.zsh.d.bak" ]]; then
+			install_rc_dir=1
+			echo "${func_name} .zshrc.d backup exists"
+		else
+			install_rc_dir=0
+			echo "backup ${HOME}/zsh.d directory.."
+			cp -rf ${HOME}/.zsh.d ${HOME}/.zsh.d.bak
+			rm -rf ${HOME}/.zsh.d
+			echo ""
+		fi
+	# backup the current .zsh.d directory if exists
+	fi
 
-# copy the new config file to home folder
-echo "copy new config files.."
-cp -f zshrc $HOME/.zshrc
-# copy the new config file to home folder
-mkdir $HOME/.zsh.d
-cp -rf zsh/* $HOME/.zsh.d/
-echo "config files added"
+	# backup the current .zshrc file if exists
+	if [ "${install_rc_file}" -eq 0 ]; then
+		# copy the new config file to home folder
+		cp -f zshrc ${HOME}/.zshrc
+		echo "zshrc: [done]"
+	else
+		echo 'zshrc: there is nothing to do'
+	fi
+
+	# backup the current .zshrc file if exists
+	if [ "${install_rc_dir}" -eq 0 ]; then
+		# copy the new config file to home folder
+		mkdir ${HOME}/.zsh.d
+		cp -rf zsh/* ${HOME}/.zsh.d/
+		echo "zshrc.d: [done]"
+	else
+		echo 'zshrc.d: there is nothing to do'
+	fi
+
+
+	if [ "${install_rc_dir}" -ne 0 ] && [ "${install_rc_file}" -ne 0 ]; then
+		echo ""
+		printf '\t%s\n' \
+		"try with -r flag to [1mremove[0m old backups" \
+		"or with -f flag to [1mremove[0m old backups and install the scripts"
+		#"try with -r flag to **remove** old backups" 
+		echo ""
+	fi
+
 # print exit status
 echo "[ Shell returned $? ]"
 
 exit 0
 }
+# }}}
 
+# help {{{
 function help () {
 cat << _EOF
 powermore-zsh plugin, a fork of powerless plugin.
@@ -55,16 +84,16 @@ Usage: [ ./make.sh ] or [ bash make.sh ]
 
 Running this script without any arguments will backup
 the current ~/.zshrc and ~/.zsh.d (if there is any already)
-and copy the new config files into your \$HOME directory.
+and copy the new config files into your \${HOME} directory.
 
 Arguments:
 	-h print this message
 	-v print version and exit
 	-r removes the backup files
-	   for having a cleaner home folder ;)
+	-f removes the backup files and installes the scripts
 
 Wanna fork it?
-		https://github.com/primejade/powermore-zsh
+		https://github.com/hossein-lap/powermore-zsh
 
 The powerless project:
 		https://github.com/martinrotter/powerless
@@ -72,28 +101,25 @@ _EOF
 
 exit 0
 }
+# }}}
 
+# rm backup {{{
 function backup_remove () {
-  if [[ -d "$HOME/.zsh.d.bak" ]]; then
+  if [[ -d "${HOME}/.zsh.d.bak" ]]; then
     echo "remove backups.."
-    rm -rf $HOME/.zsh.d.bak
-    rm -rf $HOME/.zsh.d.bak
+    rm -rf ${HOME}/.zsh.d.bak
+    rm -rf ${HOME}/.zshrc.bak
   else
     echo "You are clean, no backup file exists :)"
   fi
-
-exit 0
 }
+# }}}
 
 case $1 in
-  "") applyit
-    ;;
-  -h) help
-    ;;
-  -r) backup_remove
-    ;;
-  -v) echo "powermore ZSH plugin version ${ver_number}"
-    ;;
-   *) help
-    ;;
+  "") applyit ;;
+  -h) help ;;
+  -r) backup_remove ;;
+  -f) backup_remove; applyit ;;
+  -v) echo "powermore ZSH plugin version ${ver_number}" ;;
+   *) help ;;
 esac
